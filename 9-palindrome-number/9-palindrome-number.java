2550f2d26e67c3c22b71abdaf95fae94d48b85a8
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-08-16 15:45:33 +0530

    Time: 8 ms (99.82%), Space: 41.7 MB (94.11%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0 ||(x%10==0 && x!=0))
+        if(x<0 || (x%10==0 && x!=0)){
             return false;
-        int reversenum=0,pop;
-        while(x>reversenum){
-            pop=x%10;
-            reversenum=reversenum*10 + pop;
+        }
+        int reversedNum=0;
+        while(x>reversedNum){
+            int pop = x%10;
+            reversedNum = reversedNum*10 + pop;
             x=x/10;
         }
-        return x==reversenum || x== reversenum/10;
+        return x==reversedNum || x==reversedNum/10;
     }
 }
