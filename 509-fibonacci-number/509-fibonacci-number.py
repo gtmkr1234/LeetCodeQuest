df6eed30f79c85b623b8bbc7d97bda1ad5b46b9d
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-05 15:47:09 +0530

    Time: 62 ms (36.25%), Space: 14 MB (9.38%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,9 +1,10 @@
-
 class Solution:
+    def __init__(self):
+        self.caches = {}
     def fib(self, n: int) -> int:
-        if n<=1:
-            return n
-        if n==2:
-            return 1
-        a = self.fib(n-1) + self.fib(n-2)
-        return a
+        if n in self.caches: return self.caches[n]
+        if n == 0: return 0
+        if n <= 2: return 1
+        res = self.fib(n-1) + self.fib(n-2)
+        self.caches[n] = res
+        return res
