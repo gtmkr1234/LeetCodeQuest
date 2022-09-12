e4029eb36f12e31eebe10ea59ae19df6122e7f37
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-12 15:27:45 +0530

    Time: 5 ms (15.04%), Space: 46.4 MB (85.44%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -23,9 +23,7 @@ class Solution {
             if(l1!=null) l1=l1.next;
             if(l2!=null) l2=l2.next;
         }
-        if(carry>0){
-            tail.next = new ListNode(carry);
-        }
+        if(carry>0) tail.next = new ListNode(carry);
         return dummy.next;
     }
 }
