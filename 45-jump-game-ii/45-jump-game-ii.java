3a8b2c75ff59624f7367297a9a5d96430763bf9f
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-09-13 15:32:34 +0530

    Time: 106 ms (20.54%), Space: 49.3 MB (56.36%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -6,12 +6,10 @@ class Solution {
         dp[n-1] = 0;
         for(int i=n-2; i>=0; i--){
             int min = Integer.MAX_VALUE;
-            for(int j=i+1; j<=Math.min(n-1,i+nums[i]); j++){
+            for(int j=i+1; j<=Math.min(n-1,i+nums[i]); j++)
                 min = Math.min(min,dp[j]);
-            }
-            if(min!=Integer.MAX_VALUE){
-                dp[i] = min+1;
-            }           
+            if(min!=Integer.MAX_VALUE)
+                dp[i] = min+1;         
         }
         return dp[0];
     }
