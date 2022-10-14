c001f8b23987434aa217fba73437a47404e6acef
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-10-14 10:50:30 +0530

    Iterative Approach

---
+++ 144-Binary-Tree-Preorder-Traversal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode node) {
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> rights = new Stack<>();
+        while(node !=null){
+            list.add(node.val);
+            if(node.right !=null){
+                rights.push(node.right);
+            }
+            if(node.left ==null && !rights.isEmpty()){
+                node = rights.pop();
+            }
+            else{
+                node = node.left;
+            }
+        }
+        return list;
+    }
+}
