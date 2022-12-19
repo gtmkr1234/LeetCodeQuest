13f1fde2287139d25af6049589879ba2a314d645
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-12-19 16:39:54 +0530

    Time: 11 ms (71.78%), Space: 44.7 MB (55.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,9 +5,9 @@ class Solution {
         int len=0;
         int max = 0;
         while(right < n){
-            if(maps.containsKey(s.charAt(right))){
+            if(maps.containsKey(s.charAt(right)))
                 left = Math.max(left, maps.get(s.charAt(right))+1);
-            }
+            
             maps.put(s.charAt(right), right);
             len = Math.max(len, right-left+1);
             right++;
