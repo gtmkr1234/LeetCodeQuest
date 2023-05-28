6d6263f4cbe68605c7acbfe984bde7a505629d5a
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2023-05-28 18:30:06 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (96.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -3,18 +3,20 @@ class Solution {
         int n= nums.length;
         // Find the break pointer
         
-        int ind=-1;
+        int ind=-1; 
         for(int i=n-2; i>=0; i--){
             if(nums[i]<nums[i+1]){
                 ind =i;
                 break;
             }
         }
+        //If there is no Index, just simply reverse the list and return 
         if(ind==-1){
             reverse(nums,0);
             return;
         }
         
+        // Find the next biggest number and swap with the index
         for(int i=n-1; i>ind; i--){
             if(nums[i]>nums[ind]){
                 swap(nums,i,ind);
