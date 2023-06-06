9b07d443179e6607c2c739d63594ab75bd3b6621
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2023-06-06 15:14:02 +0530

    Time: 122 ms (39.21%), Space: 18.4 MB (41.30%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,17 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        n = len(intervals)
+        ans = []
+        intervals.sort()
+        
+        for i in range(n):
+            if not ans or intervals[i][0]>ans[-1][1]:
+                ans.append(intervals[i])
+            else:
+                ans[-1][1] = max(ans[-1][1], intervals[i][1])
+        return ans
+        
