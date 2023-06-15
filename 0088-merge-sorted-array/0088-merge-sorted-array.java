6504b009044999c128bc0ed902d603597e627a7f
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2023-06-15 11:03:53 +0530

    Time: 1 ms (33.24%), Space: 41.6 MB (70.94%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,32 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //variables to work as pointers
-        int i=m-1; // will point at m-1 index of nums1 array
-        int j=n-1; // will point at n-1 index of nums2 array
-        int k=nums1.length-1; //will point at the last position of the nums1 array
-
-        // Now traversing the nums2 array
-        while(j>=0){
-            // If element at i index of nums1 > element at j index of nums2
-            // then it is largest among two arrays and will be stored at k position of nums1
-            // using i>=0 to make sure we have elements to compare in nums1 array
-            if(i>=0 && nums1[i]>nums2[j]){
-                nums1[k]=nums1[i];
-                k--; 
-                i--; //updating pointer for further comparisons
-            }else{
-                // element at j index of nums2 array is greater than the element at i index of nums1 array 
-                // or there is no element left to compare with the nums1 array 
-                // and we just have to push the elements of nums2 array in the nums1 array.
-                nums1[k] = nums2[j];
-                k--; 
-                j--; //updating pointer for further comparisons
+        if(m>=1){
+        int left = m-1;
+        int right = 0;
+        while(left>=0 && right<n){
+            if(nums1[left]>nums2[right]){
+                int temp = nums1[left];
+                nums1[left] = nums2[right];
+                nums2[right] = temp;
+                left--;
+                right++;
+            }
+            else{
+                break;
+            }
+        }
+        Arrays.sort(nums2);
+        Arrays.sort(nums1, 0, m-1);
+        int j=0;
+        for(int i=m; i<m+n; i++){
+            nums1[i] = nums2[j];
+            j++;
+        }
+            Arrays.sort(nums1);
+        }
+        else if(n>=1){
+            for(int j=0; j<nums1.length; j++){
+                nums1[j] = nums2[j];   
             }
         }
     }
